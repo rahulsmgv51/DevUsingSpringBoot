362031ba7bea6680c1aa9d48c1d4bfd0d645fe88
rahulsmgv51 <rahulsmgv@gmail.com>
2025-05-09 08:38:27 +0530

    logging

--- geoapp/src/main/java/com/rahulsmgv/geoapp/GeoappApplication.java
+++ geoapp/src/main/java/com/rahulsmgv/geoapp/GeoappApplication.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
+//@EnableTransactionManagement
 public class GeoappApplication {
 
 	public static void main(String[] args) {
--- geoapp/src/main/java/com/rahulsmgv/geoapp/controller/PublicController.java
+++ geoapp/src/main/java/com/rahulsmgv/geoapp/controller/PublicController.java
@@ -1,21 +1,39 @@
 package com.rahulsmgv.geoapp.controller;
 
-import com.rahulsmgv.geoapp.entity.UserEntity;
 import com.rahulsmgv.geoapp.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.*;
+import java.util.logging.Logger;
 
+//@Slf4j
 //Set Profiles only run at the dev mode
 @Profile("dev")
 @RestController
 @RequestMapping("/public")
 public class PublicController {
+
+//    private static final Logger logger = (Logger) LoggerFactory.getLogger(PublicController.class);
+
     @Autowired
     UserService userService;
 
     @GetMapping("/health-check")
-    public String healthCheck(){ return "OK";}
+    public String healthCheck(){
+        try {
+//            logger.info("Hello I am checking logging module with logger ");
+            return "OK";
+        }catch (Exception ignored){
+//            log.info("Exception in the class of {}", ignored.getClass(), ignored.getMessage());
+//            log.debug("Exception in the class of {}", ignored.getClass(), ignored.getMessage());
+//            log.trace("Exception in the class of {}", ignored.getClass(), ignored.getMessage());
+//            log.warn("Exception in the class of {}", ignored.getClass(), ignored.getMessage());
+//            log.error("Exception in the class of {}", ignored.getClass(), ignored.getMessage());
+            return "NoHealth";
+        }
+    }
 
 //    @PostMapping
 //    public void createUser(@RequestBody UserEntity userEntity){
